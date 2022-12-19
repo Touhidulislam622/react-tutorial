a0f50873ca1c0a5bb6ed95a5899eb1315d33db6f
Touhidulislam622 <touhidulislam622p@gmail.com>
2022-12-19 13:18:37 +0600

    state

--- src/State.js
+++ src/State.js
@@ -5,15 +5,42 @@ export default class State extends Component {
     super(props)
   
     this.state = {
-       count : 1 
+       count : 2 
     }
   }
+
+  handleSwitch = () => {
+    this.setState({
+      count : this.state.count + 1
+    })
+  }
+         //====??//
+
+  decSwitch = () => {
+    this.setState({
+      count : this.state.count -1
+    })
+  }
+
+  //===========??//
+  
+  intoSwitch = () => {
+    this.setState({
+      count : this.state.count*2
+    })
+  }
+                  //==========??//
   
   render() {
     const {count} = this.state
     return (
       <div>
-        <h1> Count: {count}</h1>
+        <h1> serial No:  {count}</h1>
+        <button  onClick={this.handleSwitch}>+</button>
+
+        <button style= {{ margin: "10px"}} onClick={this.decSwitch}>-</button>
+        <button style= {{ margin: "10px"}} onClick={this.intoSwitch}>*</button>
+
       </div>
     )
   }
