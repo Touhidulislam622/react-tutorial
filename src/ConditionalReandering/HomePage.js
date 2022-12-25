f15d0fa1e57d23359f0416b39d964384f167d43a
Touhidulislam622 <touhidulislam622p@gmail.com>
2022-12-25 09:05:51 +0600

    dddd

--- src/ConditionalReandering/HomePage.js
+++ src/ConditionalReandering/HomePage.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
-        <h1>homePage</h1>
-      </div>
+      <h1>HomePage</h1>
     )
   }
 }
